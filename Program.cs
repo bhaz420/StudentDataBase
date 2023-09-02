9ec39198f105f94a3e20c3b5b21586ebd857d9fe
bhaz420 <127907510+bhaz420@users.noreply.github.com>
2023-09-01 20:05:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace StudentDataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDbContext())
+            {
+
+                // Add a student
+                var newStudent = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                // Retrieve and display the added student
+                var addedStudent = context.Students.Find(newStudent.Id);
+                Console.WriteLine($"Added Student: {addedStudent.FirstName} {addedStudent.LastName}");
+            }
+        }
+        public class Student
+        {
+            public int Id { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+        }
+        public class StudentDbContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+        }
+    }
+}
